6463965089b011612e8cffea6b9cf70d9025ae7e
오수향 <suhyang@osuhyangs-MacBook-Pro.local>
2021-11-29 15:33:40 +0900

    monthly code challenge season3

---
+++ Level1/나머지가_1이_되는_수_찾기/Solution.java
@@ -0,0 +1,12 @@
+package 나머지가_1이_되는_수_찾기;
+
+public class Solution {
+    public int solution(int n) {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 1) {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
