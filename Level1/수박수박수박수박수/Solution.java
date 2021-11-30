197a2212e2981e36bae454b2b412f50958f2f5c3
오수향 <suhyang@osuhyangs-MacBook-Pro.local>
2021-11-30 16:00:54 +0900

    practice question

--- Level1/수박수박수박수박수/Solution.java
+++ Level1/수박수박수박수박수/Solution.java
@@ -2,8 +2,10 @@ package 수박수박수박수박수;
 
 public class Solution {
     public String solution(int n) {
+        // return new String(new char [n/2+1]).replace("\0", "수박").substring(0,n);
         String answer = "";
         for (int i = 0; i < n; i++) {
+            // result += i % 2 == 0 ? "수" : "박";
             if (i % 2 == 0) {
                 answer += "수";
             } else {
